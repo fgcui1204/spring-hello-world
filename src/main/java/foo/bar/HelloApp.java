4ea3a9ddad9c2df54c0aa8c4cd0c2c7bf16fc4dd
fgcui <fgcui1204@163.com>
2014-11-02 12:42:54 +0800

    modify HelloApp

--- src/main/java/foo/bar/HelloApp.java
+++ src/main/java/foo/bar/HelloApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class HelloApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-        HelloService helloService = context.getBean(HelloService.class);
-        System.out.println(helloService.sayHello());
+        IService service = (IService)context.getBean("hello");
+        service.sayHello("cui");
     }
 }
